dbf6f3cd2734f2de1d597b942f5566de980a6ba4
Shane Murnaghan <murnagha@gmail.com>
2016-10-25 15:55:38 -0600

    WIP: Add unit tests for DOI service, with mocking out EZID API

--- spec/jobs/doi_create_job_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe DOICreateJob, type: :job do
-  #pending "add some examples to (or delete) #{__FILE__}"
-  #DOICreateJob.perform_now(generic_file.id)
-  #assert generic_file.reload.charged_for?(doi)
-  #TODO
-end
